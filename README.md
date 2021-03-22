08d2c80214396f9deebb3e65dc0119705e5b3162
codesquadkiki <47373509+codesquadkiki@users.noreply.github.com>
2021-03-22 00:39:22 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Course Grade Calcuation
+
+## Directions
+In this classwork assignment, you are going to write a program that will calculate and return a student's current grade percentage. Also, the program will calculate how many additional points a student needs to receive a specifc grade. The results will be outputted on the browser for student to read.
+
+## Submission
+Push your code to your Github account and then paste the link to the repo when submitting. If you do not finish in class, submit what you were able to get done.
+
+### Assignment Hints
+- Remember that `prompt` output strings. How do you convert strings to numbers?
+
+- `Math.round()` will be needed in order to return a number that has been rounded to the nearest whole number.
+
+- Use `document.write()` to display the sentences on the browser.
+
+- Don't forget that you use use template literals which allows you to embed expression inside of a string.
+
+- Remember to use the `break` keyword in a switch statement which will ensure that the program breaks out of the switch once the matched statement is executed.
+
+
+### Developer's Tricks
+- Use pseudocode to help you with creating a blueprint of the app before you start writing JavaScript. 
+
+- Store the different prompts into variables. 
+
+- Think about when you should use `let` or `const`.
+
+- Use descriptive variable/ function names.
---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+/************** PART 1 **************/
+
+// STEP 1: Write a variable that will store the total points possible (2500 points) that you can get in this course.
+
+
+// STEP 2: Use a prompt to collect the number of points a student currently has in the course and store that number in a variable.
+
+
+// STEP 3: Write an arrow function that accepts two arguments. The function should calculate and return the student's CURRENT grade percentage.
+
+
+// STEP 4: Write on the page a sentence that will say, "Your current grade is [CURRENT GRADE PERCENTAGE]%."
+
+
+
+/************** PART 2 **************/
+
+// STEP 5: Use a prompt to collect the grade that the student would like to achieve. The prompt should state, "What is the grade you want? 90, 80, 70 or 60?" Store that number in a variable.
+
+
+// STEP 6: Write an arrow function that accepts THREE arguments. This function will calculate and return the number of ADDITIONAL points the student needs to receive a specified grade. For example, if I tell the function that I have 1200 points and want to know how many points it will take to get a 90% in the course, the function will return 1050.
+
+
+// STEP 7: Write a switch statement that will write on the page a sentence. The cases depends on what the student has entered for the grade that they want. The results for each case should say, "You have [CURRENT POINTS] points. You need [ADDITIONAL POINTS NEEDED] points to earn a [GRADE WANTED]% in this course." 
+// EXAMPLE: "You have 1200 points. You need 1050 points to earn 90% in this course."
+// The default should say, "To receive a specified grade, please enter either 90, 80, 70 or 60 to find out how many additional points you need."
+
+
+
